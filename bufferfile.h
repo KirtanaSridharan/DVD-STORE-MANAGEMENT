a66549c7fad833bc737686ffaf50eab16d8031c8
Mohammed Misran <mohammed.misran38@gmail.com>
2021-07-15 00:16:19 +0530

    buffer file

--- bufferfile.h
+++ bufferfile.h
@@ -1,40 +1,57 @@
 class BufferFile {
   public:
-    BufferFile(DelimFieldBuffer &);
-    int Open(const char * filename, ios_base::openmode MODE);
+    BufferFile(IOBuffer & buffer, const string filename);
+    void Create();
+    int Open(ios_base::openmode mode);
     void Close();
     int Read(int recAddr = -1);
     int Write(int recAddr = -1);
-    int Append();
-
+    void MarkAsDeleted(int recAddr);
+  
   protected:
-    DelimFieldBuffer & Buffer;
-    fstream FILE;
+    IOBuffer & Buffer;
+    string Filename;
+    fstream File;
 };
 
-BufferFile::BufferFile(DelimFieldBuffer & buffer): Buffer(buffer) {}
+BufferFile::BufferFile(IOBuffer & buffer, const string filename): Buffer(buffer) {
+  Filename = filename;
+}
 
-int BufferFile::Open(const char * filename, ios_base::openmode MODE) {
-  FILE.open(filename, MODE);
-  return FILE.good();
+void BufferFile::Create() {
+  File.open(Filename, ios_base::in);
+  if (!File) {
+    cout << "Creating file " << Filename << "...\n";
+    File.open(Filename, ios_base::out);
+    cout << "Done!\n";
+  }
+  File.close();
+}
+
+int BufferFile::Open(ios_base::openmode mode) {
+  File.open(Filename, mode);
+  return File.good();
 }
 
 void BufferFile::Close() {
-  FILE.close();
+  File.close();
 }
 
 int BufferFile::Read(int recAddr) {
-  if (recAddr == -1) return Buffer.Read(FILE);
-  else return Buffer.DRead(FILE, recAddr);
+  if (recAddr == -1)
+    return Buffer.Read(File);
+  else
+    return Buffer.DRead(File, recAddr);
 }
 
 int BufferFile::Write(int recAddr) {
-  if (recAddr == -1) return Buffer.Write(FILE);
-  else return Buffer.DWrite(FILE, recAddr);
+  if (recAddr == -1)
+    return Buffer.Write(File);
+  else
+    return Buffer.DWrite(File, recAddr);
 }
 
-int BufferFile::Append() {
-  FILE.seekp(0, ios_base::end);
-  return Buffer.Write(FILE);
+void BufferFile::MarkAsDeleted(int recAddr) {
+  File.seekp(recAddr, ios_base::beg);
+  File.put('$');
 }
-

18b2960c36fa6b9560d4b7d45f7a5f4333d505d0
Mohammed Misran <mohammed.misran38@gmail.com>
2021-07-15 00:18:48 +0530

    test functionality

--- test.cpp
+++ test.cpp
@@ -1,28 +1,39 @@
 #include <iostream>
-#include <fstream>
-#include <cstring>
-#include <charconv>
-#include <iomanip>
+// #include <fstream>
+// #include <cstring>
+// #include <charconv>
+// #include <iomanip>
+#include <string>
 
 using namespace std;
 
-#include "delimbuffer.h"
-#include "bufferfile.h"
-#include "recfile.h"
-#include "dvd.h"
+// #include "delimbuffer.h"
+// #include "bufferfile.h"
+// #include "recfile.h"
+// #include "dvd.h"
 
-int main() {
-  Dvd dvd;
-  DelimFieldBuffer::SetDefaultDelim('|');
-  DelimFieldBuffer Buff;
-  RecordFile<Dvd> DvdFile(Buff);
+// int main() {
+//   Dvd dvd;
+//   DelimFieldBuffer::SetDefaultDelim('|');
+//   DelimFieldBuffer Buff;
+//   RecordFile<Dvd> DvdFile(Buff);
 
-  DvdFile.Open("dvd-list.txt", ios_base::in);
+//   DvdFile.Open("dvd-list.txt", ios_base::in);
 
-  int recAddr = 149;
-  int result = DvdFile.Read(dvd, recAddr);
-  cout << "Record read from address " << result << "\n";
-  cout << dvd << "\n";
-  DvdFile.Close();
+//   int recAddr = 149;
+//   int result = DvdFile.Read(dvd, recAddr);
+//   cout << "Record read from address " << result << "\n";
+//   cout << dvd << "\n";
+//   DvdFile.Close();
+//   return 0;
+// }
+
+int main ()
+{
+  string str;
+  str = "Misran|Mohammed|Jayanagar";
+  cout << str.substr(0, 6) << "\n";
+  cout << str.substr(7, 8) << "\n";
+  cout << str.substr(16, 9) << "\n";
   return 0;
 }

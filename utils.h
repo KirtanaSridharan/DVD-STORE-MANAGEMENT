8d63aa0b2d1dc616126a6ca1754f8878691438ae
Mohammed Misran <mohammed.misran38@gmail.com>
2021-07-30 00:36:31 +0530

    utility functions

--- utils.h
+++ utils.h
@@ -9,18 +9,27 @@ int validateNumStr(string value) {
 }
 
 
-int validateID(BTree & tree, string ID) {
+int validateID(BTree tree, string ID) {
   int id = validateNumStr(ID);
-  return (id > 0 && (tree.search(id) == -1));
+  if (id <= 0) {
+    cout << RED << "\nPlease enter a positive integer for ID.\n\n" << CYAN;
+    return false;
+  }
+  if (tree.search(id) == -1)
+    return true;
+  else {
+    cout << RED << "\nThis ID already exist.\n\n" << CYAN;
+    return false;
+  }
 }
 
 int validateField(const char field, string value) {
   int num = validateNumStr(value);
   int valid;
   switch (field) {
-    case 'P':
-    case 'Q': valid = (num > 0); break;
-    case 'Y': valid = (num > 0 && value.size() == 4); break;
+    case 'P': valid = (num >= 200 && num <= 5000); break;
+    case 'Q': valid = (num >= 0 && num <= 10); break;
+    case 'Y': valid = (num >= 1950 && num <= 2021); break;
   }
   return valid;
 }
